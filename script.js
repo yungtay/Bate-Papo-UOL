e3ef93c24b8f37e15375b75c2df40e67bc5b86c5
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-04-16 19:16:40 -0300

    Consertei os bugs de: todo novo refresh dos contatos voltar para Todos, se seu contato sair do chat a mensagem ira para todos, o texto do reservado é alterado de o contato sair

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const siderBar = document.querySelector(".sidebar");
 const cortina = document.querySelector(".cortina");
-let penultimaMensagem = "";
-let destinatario;
-let tipoMensagem;
-let presenca;
+const telaNome= document.querySelector(".telaEntrada")
+const mensagens = document.querySelector(".conteudo")
+const nome = document.querySelector(".telaEntrada input")
+const msg = document.querySelector(".fundo textarea")
+const contatos = document.querySelector(".escolha-contato")
+const enviar = document.querySelector(".fundo ion-icon")
+
+let destinatario = "Todos";
+let tipoMensagem = "message";
 
 function mandaNome(url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants" ){
-    const nome = document.querySelector(".telaEntrada input").value;
-    const promessa = axios.post(url, {name:nome});
+    const promessa = axios.post(url, {name:nome.value});
     return promessa;
 }
 
@@ -16,20 +20,21 @@ function entrarNaSala(promessa){
     promessa.catch(negarAcesso)
 }
 
-function autorizarAcesso(resposta){
-    const telaNome= document.querySelector(".telaEntrada")
-    telaNome.classList.add("escondido")
-    presente()
-    buscarParticipantes()
-    setInterval(requisitarConversas, 3000)
+function negarAcesso(erro){
+    alert(`Nome sendo utilizado, por favor escolha outro`)
 }
 
-function negarAcesso(erro){
-    alert(`O erro ${erro.response.status} ocorreu, por favor escolha outro nome`)
+function autorizarAcesso(resposta){
+    telaNome.classList.add("escondido")
+    presente()
+    requisitarConversas()
+    setInterval(requisitarConversas, 3000)
+    buscarParticipantes()
+    setInterval(buscarParticipantes, 10000)
 }
 
 function presente(){
-    presenca = setInterval(mandaNome, 5000, "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status");
+    setInterval(mandaNome, 5000, "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status");
 }
 
 function requisitarConversas(){
@@ -38,76 +43,99 @@ function requisitarConversas(){
     promessa.catch(tratarErro)
 }
 
-function popularMensagens(reposta){
-    const mensagens = document.querySelector(".conteudo")
-    const resposta = reposta.data
-    typeof(resposta)
+function popularMensagens(resposta){
+    const dados = resposta.data
     mensagens.innerHTML = ""
-    Object.entries(resposta).forEach(([key]) => {
-        if(resposta[key].type === "status"){
-            mensagens.innerHTML += `<div class="caixa-texto ${resposta[key].type}"><span><span class="tempo">(${resposta[key].time})</span><strong>${resposta[key].from}</strong> ${resposta[key].text}</span></div>`
-        } else if(resposta[key].type === "message") {
-            mensagens.innerHTML += `<div class="caixa-texto ${resposta[key].type}"><span><span class="tempo">(${resposta[key].time})</span><strong>${resposta[key].from}</strong> para <strong>${resposta[key].to}:</strong> ${resposta[key].text}</span>
+    for(key in dados){
+        if(dados[key].type === "status"){
+            mensagens.innerHTML += `<div class="caixa-texto ${dados[key].type}"><span><span class="tempo">(${dados[key].time})</span><strong>${dados[key].from}</strong> ${dados[key].text}</span></div>`
+        } else if(dados[key].type === "message") {
+            mensagens.innerHTML += `<div class="caixa-texto ${dados[key].type}"><span><span class="tempo">(${dados[key].time})</span><strong>${dados[key].from}</strong> para <strong>${dados[key].to}:</strong> ${dados[key].text}</span>
             </div>`
-        } else if(resposta[key].type === "private_message" && resposta[key].to === document.querySelector(".telaEntrada input").value){
-            mensagens.innerHTML += `<div class="caixa-texto ${resposta[key].type}"><span><span class="tempo">(${resposta[key].time})</span><strong>${resposta[key].from}</strong> reservadamente para <strong>${resposta[key].to}:</strong> ${resposta[key].text}</span>
+        } else if(dados[key].type === "private_message" && dados[key].to === document.querySelector(".telaEntrada input").value){
+            mensagens.innerHTML += `<div class="caixa-texto ${dados[key].type}"><span><span class="tempo">(${dados[key].time})</span><strong>${dados[key].from}</strong> reservadamente para <strong>${dados[key].to}:</strong> ${dados[key].text}</span>
             </div>`
         }
-    });
-    mensagemFinal()
+    };
 
-}
-
-function mensagemFinal(){
     const ultimaMensagem = document.querySelector('.caixa-texto:last-child');
     ultimaMensagem.scrollIntoView()
 }
 
 function tratarErro(resposta){
-    console.log(resposta.response.status)
+    alert(`O erro ${resposta.response.status} ocorreu, não foi possível obter as mensagens, falar com o Pedrão`)
+    window.location.reload()
 }
 
 function mandarMensagem(){
-    let dados = {
-        from: document.querySelector(".telaEntrada input").value,
-        to: destinatario,
-        text: document.querySelector(".fundo input").value,
-        type: tipoMensagem
+    if(msg.value !== ""){
+        let dados = {
+            from: nome.value,
+            to: destinatario,
+            text: msg.value,
+            type: tipoMensagem
+        }
+        const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages", dados)
+        promessa.then(requisitarConversas)
+        promessa.catch(foiDesconectado)
+        msg.value = ""
     }
-    const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages", dados)
-    console.log(dados)
-    promessa.then(requisitarConversas)
-    promessa.catch()
+}
+
+function foiDesconectado(resposta){
+    alert("Você foi desconectado por inatividade")
+    window.location.reload()
 }
 
 function buscarParticipantes(){
     const promessa = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants")
     promessa.then(popularSiderbar)
-    
 }
 
-function popularSiderbar(respota){
-    const resposta = respota.data
-    console.log(resposta)
-    const sidebar = document.querySelector(".escolha-contato")
-    sidebar.innerHTML = `
-    <div class="contato-sidebar" >
+function popularSiderbar(resposta){
+    let destinatarioPresente = false;
+    const dados = resposta.data
+    contatos.innerHTML = `
+    <div class="contato-sidebar">
         <ion-icon name="people"></ion-icon>
         <div class="contato-indiv" onclick="selecionarContato(this)">
             <span>Todos</span>
-            <ion-icon name="checkmark-sharp" class="escondido"></ion-icon>
+            <ion-icon name="checkmark-sharp" id="todosIon"></ion-icon>
         </div>
     </div>`
-    Object.entries(resposta).forEach(([key]) => {
-        sidebar.innerHTML +=
-        `<div class="contato-sidebar" >
-        <ion-icon name="people"></ion-icon>
-        <div class="contato-indiv" onclick="selecionarContato(this)">
-            <span>${resposta[key].name}</span>
-            <ion-icon name="checkmark-sharp" class="escondido"></ion-icon>
-        </div>
-        </div>`
-    });    
+
+    for(key in dados) {
+
+        if(destinatario === dados[key].name){
+            contatos.innerHTML +=
+            `<div class="contato-sidebar">
+            <ion-icon name="people"></ion-icon>
+            <div class="contato-indiv" onclick="selecionarContato(this)">
+                <span>${dados[key].name}</span>
+                <ion-icon name="checkmark-sharp"></ion-icon>
+            </div>
+            </div>`
+            document.querySelector("#todosIon").classList.add("escondido")
+            destinatarioPresente = true;
+
+
+        } else{
+            contatos.innerHTML +=
+            `<div class="contato-sidebar" >
+            <ion-icon name="people"></ion-icon>
+            <div class="contato-indiv" onclick="selecionarContato(this)">
+                <span>${dados[key].name}</span>
+                <ion-icon name="checkmark-sharp" class="escondido"></ion-icon>
+            </div>
+            </div>`
+        }
+    };  
+    if(destinatarioPresente === false && tipoMensagem === "private_message"){
+        destinatario = "Todos"
+        mensagemPrivada()
+    }   else if (destinatarioPresente === false) {
+        destinatario = "Todos"
+    }
 }
 
 function abrirMenu(){
@@ -117,15 +145,27 @@ function abrirMenu(){
 
 function selecionarContato(el){
     const classe= el.className
-    if(classe === "contato-indiv"){
-        destinatario = el.firstElementChild.innerHTML
-    } else if(el.firstElementChild.innerHTML === "Público"){
+    destinatario = el.firstElementChild.innerHTML
+    if(tipoMensagem === "private_message"){
+        mensagemPrivada()
+    }
+    const contatoSelecionado = document.querySelectorAll(`.contato-indiv > ion-icon`)
+    contatoSelecionado.forEach(element => {
+    element.classList.add("escondido")
+    });
+    el.lastElementChild.classList.remove("escondido")
+}
+
+function selecionarVisibilidade(el){
+    if(el.firstElementChild.innerHTML === "Público"){
         tipoMensagem = "message"
+        mensagemPublica()
     } else{
         tipoMensagem = "private_message"
+        mensagemPrivada()
     }
-    let contatoSelecionado = document.querySelectorAll(`.${classe} > ion-icon`)
-    contatoSelecionado.forEach(element => {
+    const visibilidadeSelecionada = document.querySelectorAll(`.visibilidade-indiv > ion-icon`)
+    visibilidadeSelecionada.forEach(element => {
     element.classList.add("escondido")
     });
     el.lastElementChild.classList.remove("escondido")
@@ -135,6 +175,24 @@ function telaInicial(){
     siderBar.classList.add("escondido")
     cortina.classList.add("escondido")
 }
+
+function mensagemPublica(){
+    msg.setAttribute("placeholder", 
+    "Escreva aqui...")  
+}
+
+function mensagemPrivada(){
+    msg.setAttribute("placeholder", 
+    `Escreva aqui...
+Enviando para ${destinatario} (reservadamente)`)  
+}
+
+msg.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        enviar.click();
+    }
+});
 
 
 
